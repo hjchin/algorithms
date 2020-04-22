735d79501947f2f164a4364d6cb39f2505694fdf
Hoong Jhang Chin <hj_chin@hotmail.com>
2020-04-22 21:45:46 +0800

    The Grid Search

--- src/implementation/TheGridSearch.java
+++ src/implementation/TheGridSearch.java
@@ -1,32 +1,41 @@
 package implementation;
 
+import java.util.ArrayList;
+
 public class TheGridSearch {
 
     public static String run(String[] G, String[] P){
 
-        int pi=0;
-        int i=0;
-        int l=0;
-        for(;i<G.length;i++){
-            l = G[i].indexOf(P[pi]);
-            if(l > -1){
-                break;
+        ArrayList<Integer> r = new ArrayList<>();
+        ArrayList<Integer> c = new ArrayList<>();
+        for(int i=0,l=0;i<G.length;i++, l=0){
+            while(G[i].indexOf(P[0], l) > -1){
+                l = G[i].indexOf(P[0], l);
+                r.add(i);
+                c.add(l);
+                l++;
             }
         }
 
-        if(l == -1){
+        if(r.size() == 0){
             return "NO";
         }
 
-        for(i=i+1,pi=pi+1;i<G.length && pi < P.length;i++,pi++){
-            int len = P[pi].length();
-            int index = l+len;
-            if(!G[i].substring(l,index).equals(P[pi])){
-                return "NO";
+        for(int j=0;j<r.size();j++){
+            int matchRow = 1;
+            for(int i=r.get(j)+1,pi=1;i<G.length && pi < P.length;i++,pi++){
+                int len = P[pi].length();
+                int index = c.get(j)+len;
+                if(!G[i].substring(c.get(j),index).equals(P[pi])){
+                    break;
+                }else{
+                    matchRow++;
+                }
             }
+            if(matchRow == P.length) return "YES";
         }
 
-        return "YES";
+        return "NO";
 
     }
 }
