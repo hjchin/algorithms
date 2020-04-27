b5cad597118c892389224d9b2cd1913d9f308a4e
Hoong Jhang Chin <hj_chin@hotmail.com>
2020-04-27 23:30:39 +0800

    common child

--- src/strings/CommonChild.java
+++ src/strings/CommonChild.java
@@ -6,79 +6,17 @@ public class CommonChild {
 
     public static int run(String s1, String s2){
 
-        int maxLength = 0;
-
-        for(int i=0;i<s1.length();i++){
-            ArrayList<Integer> substringIndex = new ArrayList<>();
-            substringIndex.add(i);
-            maxLength = Math.max(maxLength, findMaxChildLength(substringIndex, s1, s2, 0));
-        }
-
-        return maxLength;
-    }
-
-    public static String cc = "";
-
-    public static int findMaxChildLength(List<Integer> indices, String s1, String s2, int maxLength){
-
-        if(indices.size() == 0) return 0;
-
-        if(isCommonChild(indices, s1, s2)){
-            String child = getString(indices, s1);
-            if(child.length()> cc.length()) {
-                cc = child;
-            }
-            maxLength = Math.max(maxLength, indices.size());
-
-            for(int i = indices.get(indices.size()-1)+1;i<s1.length();i++){
-                ArrayList<Integer> a = copyArray(indices);
-                a.add(i);
-                int len = findMaxChildLength(a, s1, s2, maxLength);
-                maxLength = Math.max(maxLength, len);
-            }
-        }
-
-        return maxLength;
-    }
-
-    public static String getString(List<Integer> list, String s1){
-        StringBuilder sb = new StringBuilder();
-        for(int i: list){
-            sb.append(s1.charAt(i));
-        }
-        return sb.toString();
-    }
-
-    public static ArrayList<Integer> copyArray(List<Integer> array){
-        ArrayList<Integer> a = new ArrayList<Integer>();
-        for(Integer v: array){
-            a.add(v);
-        }
-        return a;
-    }
-
-    public static boolean isCommonChild(List<Integer> substringIndex, String s1, String s2){
-
-        if(substringIndex.size() == 0){
-            return true;
-        }
-
-        int match = 0;
-        int lastIndex = 0;
-        for(int i : substringIndex){
-            for(int j=lastIndex;j<s2.length();j++){
-                if(compare(s1, i, s2, j)){
-                    match++;
-                    lastIndex = j+1;
-                    break;
+        int[][] map = new int[s1.length()+1][s2.length()+1];
+        for(int i=1;i<s1.length()+1;i++){
+            for(int j=1;j<s2.length()+1;j++){
+                if(s1.charAt(i-1) == s2.charAt(j-1)){
+                    map[i][j] = map[i-1][j-1] + 1;
+                }else{
+                    map[i][j] = Math.max(map[i][j-1], map[i-1][j]);
                 }
             }
         }
 
-        return match == substringIndex.size();
-    }
-
-    public static boolean compare(String s1, int i1, String s2, int i2){
-        return s1.charAt(i1) == s2.charAt(i2);
+        return map[s1.length()][s2.length()];
     }
 }
--- src/strings/CommonChildTest.java
+++ src/strings/CommonChildTest.java
@@ -7,89 +7,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import static org.junit.Assert.*;
-import static strings.CommonChild.isCommonChild;
 
 public class CommonChildTest {
 
-    @Test
-    public void testIsCommonChild(){
-        String s1 = "ABCD";
-        String s2 = "ABDC";
-
-        ArrayList<Integer> indices = new ArrayList<>();
-        indices.add(0);
-        assertTrue(isCommonChild(indices, s1, s2));
-
-        indices.add(1);
-        assertTrue(isCommonChild(indices, s1, s2));
-
-        indices.add(2);
-        assertTrue(isCommonChild(indices, s1, s2));
-
-        indices = new ArrayList<>();
-        indices.add(0);indices.add(1);indices.add(3);
-        assertTrue(isCommonChild(indices, s1, s2));
-
-        indices = new ArrayList<>();
-        indices.add(0);indices.add(2);indices.add(3);
-        assertFalse(isCommonChild(indices, s1, s2));
-
-        indices = new ArrayList<>();
-        indices.add(2);indices.add(3);
-        assertFalse(isCommonChild(indices, s1, s2));
-    }
-
-    @Test
-    public void testIsCommonChild2(){
-        String s1 = "abaacc";
-        String s2 = "abbbaccc";
-
-        ArrayList<Integer> indices = new ArrayList<>();
-        indices.add(1);indices.add(2);
-        assertTrue(isCommonChild(indices, s1, s2));
-
-        indices = new ArrayList<>();
-        indices.add(1);indices.add(2);indices.add(3);
-        assertFalse(isCommonChild(indices, s1, s2));
-    }
-
-    @Test
-    public void testIsCommonChild3(){
-        String s1 = "abaaddcc";
-        String s2 = "abbbaccc";
-        ArrayList<Integer> indices = new ArrayList<>();
-        indices.add(0);indices.add(2);indices.add(3);
-        assertFalse(isCommonChild(indices, s1, s2));
-
-        indices = new ArrayList<>();
-        indices.add(0);indices.add(2);indices.add(4);
-        assertFalse(isCommonChild(indices, s1, s2));
-    }
-
-    @Test
-    public void testRecursive(){
-        String s1 = "abaaddcc";
-        String s2 = "abbbaccc";
-        ArrayList<Integer> indices = new ArrayList<>();
-        indices.add(6);
-        int maxLen = CommonChild.findMaxChildLength(indices, s1, s2, 0);
-        assertEquals("cc",CommonChild.cc);
-        assertEquals(2,maxLen);
-
-        CommonChild.cc = "";
-        indices = new ArrayList<>();
-        indices.add(5);
-        maxLen = CommonChild.findMaxChildLength(indices, s1, s2, 0);
-        assertEquals("",CommonChild.cc);
-        assertEquals(0,maxLen);
-
-        CommonChild.cc = "";
-        indices = new ArrayList<>();
-        indices.add(3);
-        maxLen = CommonChild.findMaxChildLength(indices, s1, s2, 0);
-        assertEquals("acc",CommonChild.cc);
-        assertEquals(3,maxLen);
-    }
 
     @Test
     public void test(){
