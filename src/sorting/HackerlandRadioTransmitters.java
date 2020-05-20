15f454135e99d3dcecfa124c81fb0f341a86da85
Hoong Jhang Chin <hj_chin@hotmail.com>
2020-05-20 22:33:55 +0800

    HackerlandRadioTransmitter

---
+++ src/sorting/HackerlandRadioTransmitters.java
@@ -0,0 +1,43 @@
+package sorting;
+
+import java.util.*;
+
+public class HackerlandRadioTransmitters {
+
+    public static int run(int[] x, int k){
+
+        if(x.length == 0) return 0;
+
+        Arrays.sort(x);
+
+        int radioCount = 0;
+        int lastRadio = x[0];
+        int i=0;
+
+        while(i<x.length){
+
+            while(i< x.length && x[i] <= lastRadio+k){
+                i++;
+            }
+
+            radioCount++;
+
+            if(i<x.length){
+                lastRadio = x[--i];
+                System.out.print(" "+lastRadio+",");
+            }else{
+                System.out.print(" "+x[i-1]+",");
+            }
+
+            while(i< x.length && x[i] <= lastRadio+k){
+                i++;
+            }
+
+            if(i<x.length){
+                lastRadio = x[i];
+            }
+        }
+
+        return radioCount;
+    }
+}
---
+++ src/sorting/HackerlandRadioTransmittersTest.java
@@ -0,0 +1,30 @@
+package sorting;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import static org.junit.Assert.*;
+
+public class HackerlandRadioTransmittersTest {
+
+    @Test
+    public void run() {
+        //1,2,3,4,5
+        assertEquals(2, HackerlandRadioTransmitters.run(new int[]{1, 2, 3, 4, 5}, 1));
+    }
+
+    @Test
+    public void run2() {
+        //2 4 5 6 7 9 11 12
+        assertEquals(3, HackerlandRadioTransmitters.run(new int[]{7, 2, 4, 6, 5, 9, 12, 11}, 2));
+    }
+
+    @Test
+    public void run4() {
+        //2 4 5 6 7 9 11 12
+        assertEquals(2, HackerlandRadioTransmitters.run(new int[]{7, 2, 4, 6, 5, 9, 12, 11}, 3));
+    }
+}
