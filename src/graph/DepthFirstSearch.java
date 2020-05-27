3d5770ce8212d08710b45eac43a0dfd7f3ff33aa
Hoong Jhang Chin <hj_chin@hotmail.com>
2020-05-27 11:36:35 +0800

    Depth first search

---
+++ src/graph/DepthFirstSearch.java
@@ -0,0 +1,48 @@
+package graph;
+
+import java.util.Stack;
+
+public class DepthFirstSearch {
+
+    Graph g;
+    int root;
+    int[] edgeTo;
+    boolean[] marked;
+
+    DepthFirstSearch(Graph g, int s){
+        this.g = g;
+        this.root = s;
+        edgeTo = new int[g.V()];
+        marked = new boolean[g.V()];
+        dps(g, s);
+    }
+
+    private void dps(Graph g, int s){
+        marked[s] = true;
+        for(int i:g.getAdj(s)){
+            if(!marked[i]){
+                dps(g, i);
+                edgeTo[i] = s;
+            }
+        }
+    }
+
+    public String pathTo(int x){
+
+        if(!marked[x]) return null;
+
+        Stack<Integer> path = new Stack<Integer>();
+        while(x != root){
+            path.push(x);
+            x = edgeTo[x];
+        }
+        path.push(root);
+
+        StringBuilder sb = new StringBuilder();
+        while(!path.isEmpty()){
+            sb.append(path.pop());
+            if(!path.isEmpty()) sb.append(">");
+        }
+        return sb.toString();
+    }
+}
---
+++ src/graph/DepthFirstSearchTest.java
@@ -0,0 +1,36 @@
+package graph;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DepthFirstSearchTest {
+
+    @Test
+    public void test(){
+        /*
+
+            0 - 1 - 2 - 7 - 8
+            |   |
+            4   3 - 6   9
+                |
+                5
+         */
+        Graph g = new Graph(10);
+        g.addEdge(0,1);
+        g.addEdge(0,4);
+        g.addEdge(1,2);
+        g.addEdge(1,3);
+        g.addEdge(3,5);
+        g.addEdge(3,6);
+        g.addEdge(2,7);
+        g.addEdge(7,8);
+
+        DepthFirstSearch dps = new DepthFirstSearch(g, 0);
+        assertEquals("0", dps.pathTo(0));
+        assertEquals("0>1>2>7>8", dps.pathTo(8));
+        assertEquals("0>1>3>6", dps.pathTo(6));
+        assertEquals(null, dps.pathTo(9));
+    }
+
+}
