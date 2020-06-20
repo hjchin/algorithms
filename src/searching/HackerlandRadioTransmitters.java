8fc92612e43d07bfb74c827f01bf6ed8b6644160
Hoong Jhang Chin <hj_chin@hotmail.com>
2020-06-20 20:54:29 +0800

    Search questions

--- src/searching/HackerlandRadioTransmitters.java
+++ src/searching/HackerlandRadioTransmitters.java
@@ -2,6 +2,14 @@ package searching;
 
 import java.util.*;
 
+/*
+Hackerland is a one-dimensional city with houses aligned at integral locations along a road. The Mayor wants to install radio transmitters on the roofs of the city's houses. Each transmitter has a fixed range meaning it can transmit a signal to all houses within that number of units distance away.
+
+Given a map of Hackerland and the transmission range, determine the minimum number of transmitters so that every house is within range of at least one transmitter. Each transmitter must be installed on top of an existing house.
+
+For example, assume houses are located at  and the transmission range .  antennae at houses  and  and  would provide complete coverage. There is no house at location  to cover both  and . Ranges of coverage, are , , and .
+
+ */
 public class HackerlandRadioTransmitters {
 
     public static int run(int[] x, int k){
